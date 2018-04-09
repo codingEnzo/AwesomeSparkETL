ab33dea320e1887ebb3ee7d495efffd42b5ff960
Gssssssssy <coder.gsy@gmail.com>
2018-04-09 11:15:38 +0800

    Xuzhou

---
+++ Src/SparkETLCore/CityCore/Xuzhou/ProjectCore.py
@@ -0,0 +1,198 @@
+# coding = utf-8
+from Utils import Var, Meth, Config
+
+
+def recordtime(data):
+    return data
+
+
+def projectName(data):
+    return Meth.cleanName(str(data))
+
+
+def promotionName(data):
+    return data
+
+
+def realestateProjectId():
+    pass
+
+
+def projectUUID():
+    pass
+
+
+def districtName():
+    pass
+
+
+def regionName():
+    pass
+
+
+def projectAddress():
+    pass
+
+
+def projectType():
+    pass
+
+
+def onSaleState():
+    pass
+
+
+def landUse():
+    pass
+
+
+def housingCount():
+    pass
+
+
+def developer():
+    pass
+
+
+def floorArea():
+    pass
+
+
+def totalBuidlingArea():
+    pass
+
+
+def buildingType():
+    pass
+
+
+def houseUseType():
+    pass
+
+
+def propertyRightsDescription():
+    pass
+
+
+def projectApproveData():
+    pass
+
+
+def projectBookingData():
+    pass
+
+
+def lssueDate():
+    pass
+
+
+def presalePermitNumber():
+    pass
+
+
+def houseBuildingCount():
+    pass
+
+
+def approvalPresaleAmount():
+    pass
+
+
+def approvalPresaleArea():
+    pass
+
+
+def averagePrice():
+    pass
+
+
+def earliestStartDate():
+    pass
+
+
+def completionDate():
+    pass
+
+
+def earliestOpeningTime():
+    pass
+
+
+def latestDeliversHouseDate():
+    pass
+
+
+def presaleRegistrationManagementDepartment():
+    pass
+
+
+def landLevel():
+    pass
+
+
+def greeningRate():
+    pass
+
+
+def floorAreaRatio():
+    pass
+
+
+def managementFees():
+    pass
+
+
+def managementCompany():
+    pass
+
+
+def otheRights():
+    pass
+
+
+def certificateOfUseOfStateOwnedLand():
+    pass
+
+
+def constructionPermitNumber():
+    pass
+
+
+def qualificationNumber():
+    pass
+
+
+def landUsePermit():
+    pass
+
+
+def buildingPermit():
+    pass
+
+
+def legalPersonNumber():
+    pass
+
+
+def legalPerson():
+    pass
+
+
+def sourceUrl():
+    pass
+
+
+def decoration():
+    pass
+
+
+def parkingSpaceAmount():
+    pass
+
+
+def remarks():
+    pass
+
+
+def extraJSON():
+    pass
