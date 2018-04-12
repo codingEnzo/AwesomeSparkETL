3a76feefb3a98d28ad1a3320bf54a17529974c2a
chiufung <chiufung@localhost.localdomain>
2018-04-12 14:25:41 +0800

    spark map 爬坑完毕

--- Src/SparkETLCore/Utils/Meth.py
+++ Src/SparkETLCore/Utils/Meth.py
@@ -1,8 +1,24 @@
-# coding = utf-8
+# coding=utf-8
+import json
+import demjson
+import Var
+
+
+def jsonLoad(x):
+    return demjson.decode(x)
+
+
+def jsonDumps(x):
+    return json.dumps(x, ensure_ascii=False)
 
 
 def cleanName(x):
-    return x.replace('（', '(').replace('）', ')')\
+    x = x.encode('utf-8').replace('（', '(').replace('）', ')')\
         .replace('】', ']').replace('【', '[')\
         .replace('，', ',').replace('－', '-').\
-        replace('〔', '[').replace('〕', ']') if x else ''
+        replace('〔', '[').replace('〕', ']').decode('utf-8')
+    return x
+
+
+def numberTable(x):
+    return Var.NUMTAB.get(x, '')
--- Src/SparkETLCore/Utils/Var.py
+++ Src/SparkETLCore/Utils/Var.py
@@ -1,1 +1,5 @@
-# coding = utf-8
+# coding=utf-8
+from sqlalchemy import *
+ENGINE = create_engine(
+    'mysql+pymysql://root:gh001@10.30.1.70:3307/spark_test?charset=utf8')
+NUMTAB = {ord(f): ord(t) for f, t in zip('1234567890', '一二三四五六七八九')}
