6d8d56a260cfbaf2a513e870e8f46121bbe13aa9
Gssssssssy <coder.gsy@gmail.com>
2018-04-20 11:46:17 +0800

    Completed Project

--- Src/SparkETLCore/CityCore/Xuzhou/ProjectCore.py
+++ Src/SparkETLCore/CityCore/Xuzhou/ProjectCore.py
@@ -171,6 +171,9 @@ def floorArea(data):
             axis=1)
         g = query.groupby(
             ['ExtraLandCertificate'])['ExtraFloorArea'].max().sum()
+        print('----> ')
+        print(g, '\n', query)
+        print('<---- \n')
         data['FloorArea'] = round(float(g), 2)
     data = Row(**data)
 
