ca2ef94596a84a7c7d9a1e1024b62825a390ecd9
Gssssssssy <coder.gsy@gmail.com>
2018-04-26 15:38:49 +0800

    Completed House

--- Src/SparkETLCore/CityCore/Xuzhou/HouseCore.py
+++ Src/SparkETLCore/CityCore/Xuzhou/HouseCore.py
@@ -119,7 +119,6 @@ def floorName(data):
     floor_name = Meth.getFloor(house_name)
     data['FloorName'] = floor_name
     data['HouseNumber'] = house_name
-    print(data['UnitName'])
     _ = data['UnitName'] + '单元' \
         + floor_name + '层' + house_name
     data['HouseName'] = _
