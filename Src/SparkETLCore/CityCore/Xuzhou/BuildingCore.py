72b9686413180e2d848d4b7dd4880b2061bd474f
Gssssssssy <coder.gsy@gmail.com>
2018-05-02 18:13:19 +0800

    Completed Project

--- Src/SparkETLCore/CityCore/Xuzhou/BuildingCore.py
+++ Src/SparkETLCore/CityCore/Xuzhou/BuildingCore.py
@@ -7,7 +7,6 @@ import sys
 
 import demjson
 import pandas as pd
-from pyspark.sql import Row
 
 from Utils import Meth
 
@@ -28,11 +27,9 @@ METHODS = [
 
 def recordTime(data):
     print(inspect.stack()[0][3])
-    data = data.asDict()
     if not data.get("RecordTime"):
         nt = datetime.datetime.now()
         data['RecordTime'] = nt
-    data = Row(**data)
     return data
 
 
@@ -49,11 +46,9 @@ def buildingName(data):
 
 
 def buildingId(data):
-    data = data.asDict()
     extraj = data.get("ExtraJson")
     if extraj:
         data['BuildingId'] = demjson.decode(extraj).get("ExtraBuildingID", "")
-    data = Row(**data)
     return data
 
 
@@ -62,7 +57,6 @@ def buildingUUID(data):
 
 
 def unitName(data):
-    data = data.asDict()
     b_uuid = data['BuildingUUID']
     sql = "SELECT HouseInfoItem.UnitName FROM HouseInfoItem " \
         "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
@@ -71,7 +65,6 @@ def unitName(data):
         _ = list(set(query['UnitName']))
         data['UnitName'] = demjson.encode(_)
 
-    data = Row(**data)
     return data
 
 
@@ -88,35 +81,31 @@ def address(data):
 
 
 def onTheGroundFloor(data):
-    data = data.asDict()
-    b_uuid = data['BuildingUUID']
-    sql = "SELECT HouseInfoItem.MeasuredBuildingArea, HouseInfoItem.HouseName FROM HouseInfoItem " \
-        "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
-    query = pd.read_sql(sql, ENGINE)
-    if not query.empty:
-        query['Floor'] = query.apply(
-            lambda x: Meth.getFloor(x['HouseName']), axis=1)
-        _ = query['MeasuredBuildingArea'][query['Floor'] > 0].count()
-        data['OnTheGroundFloor'] = str(_)
+    # b_uuid = data['BuildingUUID']
+    # sql = "SELECT HouseInfoItem.MeasuredBuildingArea, HouseInfoItem.HouseName FROM HouseInfoItem " \
+    #     "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
+    # query = pd.read_sql(sql, ENGINE)
+    # if not query.empty:
+    #     query['Floor'] = query.apply(
+    #         lambda x: Meth.getFloor(x['HouseName']), axis=1)
+    #     _ = query['MeasuredBuildingArea'][query['Floor'] > 0].count()
+    #     data['OnTheGroundFloor'] = str(_)
 
-    data = Row(**data)
     return data
 
 
 def theGroundFloor(data):
-    data = data.asDict()
-    b_uuid = data['BuildingUUID']
-    sql = "SELECT HouseInfoItem.MeasuredBuildingArea, HouseInfoItem.HouseName FROM HouseInfoItem " \
-        "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
-    query = pd.read_sql(sql, ENGINE)
-    if not query.empty:
-        query['Floor'] = query.apply(
-            lambda x: Meth.getFloor(x['HouseName']), axis=1)
-        _ = query['MeasuredBuildingArea'][(query['Floor'] < 1) & (query['Floor'] != 0)] \
-            .count()
-        data['OnTheGroundFloor'] = str(_)
+    # b_uuid = data['BuildingUUID']
+    # sql = "SELECT HouseInfoItem.MeasuredBuildingArea, HouseInfoItem.HouseName FROM HouseInfoItem " \
+    #     "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
+    # query = pd.read_sql(sql, ENGINE)
+    # if not query.empty:
+    #     query['Floor'] = query.apply(
+    #         lambda x: Meth.getFloor(x['HouseName']), axis=1)
+    #     _ = query['MeasuredBuildingArea'][(query['Floor'] < 1) & (query['Floor'] != 0)] \
+    #         .count()
+    #     data['OnTheGroundFloor'] = str(_)
 
-    data = Row(**data)
     return data
 
 
@@ -125,32 +114,28 @@ def estimatedCompletionDate(data):
 
 
 def housingCount(data):
-    data = data.asDict()
-    b_uuid = data['BuildingUUID']
-    sql = "SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem " \
-        "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
-    query = pd.read_sql(sql, ENGINE)
-    if not query.empty:
-        data['HousingCount'] = str(query['MeasuredBuildingArea'].count())
-    data = Row(**data)
+    # b_uuid = data['BuildingUUID']
+    # sql = "SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem " \
+    #     "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
+    # query = pd.read_sql(sql, ENGINE)
+    # if not query.empty:
+    #     data['HousingCount'] = str(query['MeasuredBuildingArea'].count())
     return data
 
 
 def floors(data):
-    data = data.asDict()
-    b_uuid = data['BuildingUUID']
-    sql = "SELECT HouseInfoItem.HouseName FROM HouseInfoItem " \
-        "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
-    query = pd.read_sql(sql, ENGINE)
-    if not query.empty:
-        try:
-            query['Floor'] = query.apply(
-                lambda x: int(Meth.getFloor(x['HouseName'])), axis=1)
-            data['Floor'] = str(query['Floor'].count())
-        except ValueError:
-            print('BuildingUUID: ', b_uuid)
+    # b_uuid = data['BuildingUUID']
+    # sql = "SELECT HouseInfoItem.HouseName FROM HouseInfoItem " \
+    #     "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
+    # query = pd.read_sql(sql, ENGINE)
+    # if not query.empty:
+    #     try:
+    #         query['Floor'] = query.apply(
+    #             lambda x: int(Meth.getFloor(x['HouseName'])), axis=1)
+    #         data['Floor'] = str(query['Floor'].count())
+    #     except ValueError:
+    #         print('BuildingUUID: ', b_uuid)
 
-    data = Row(**data)
     return data
 
 
@@ -167,33 +152,29 @@ def elevaltorInfo(data):
 
 
 def buildingStructure(data):
-    data = data.asDict()
-    b_uuid = data['BuildingUUID']
-    sql = "SELECT HouseInfoItem.BuildingStructure FROM HouseInfoItem " \
-        "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
-    query = pd.read_sql(sql, ENGINE)
-    if not query.empty:
-        _ = list(
-            set(query['BuildingStructure'][query['BuildingStructure'] != ""]))
-        _ = list(map(lambda i: Meth.cleanBuildingStructure(i), _))
-        data['BuildingStructure'] = demjson.encode(_)
+    # b_uuid = data['BuildingUUID']
+    # sql = "SELECT HouseInfoItem.BuildingStructure FROM HouseInfoItem " \
+    #     "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
+    # query = pd.read_sql(sql, ENGINE)
+    # if not query.empty:
+    #     _ = list(
+    #         set(query['BuildingStructure'][query['BuildingStructure'] != ""]))
+    #     _ = list(map(lambda i: Meth.cleanBuildingStructure(i), _))
+    #     data['BuildingStructure'] = demjson.encode(_)
 
-    data = Row(**data)
     return data
 
 
 def buildingType(data):
-    data = data.asDict()
-    b_uuid = data['BuildingUUID']
-    sql = "SELECT HouseInfoItem.HouseName FROM HouseInfoItem " \
-        "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
-    query = pd.read_sql(sql, ENGINE)
-    if not query.empty:
-        query['Floor'] = query.apply(
-            lambda x: Meth.getFloor(x['HouseName']), axis=1)
-        _ = Meth.bisectCheckFloorType(query['Floor'].max())
-        data['BuildingType'] = _
-    data = Row(**data)
+    # b_uuid = data['BuildingUUID']
+    # sql = "SELECT HouseInfoItem.HouseName FROM HouseInfoItem " \
+    #     "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
+    # query = pd.read_sql(sql, ENGINE)
+    # if not query.empty:
+    #     query['Floor'] = query.apply(
+    #         lambda x: Meth.getFloor(x['HouseName']), axis=1)
+    #     _ = Meth.bisectCheckFloorType(query['Floor'].max())
+    #     data['BuildingType'] = _
 
     return data
 
@@ -223,18 +204,16 @@ def buildingPriceRange(data):
 
 
 def buildingArea(data):
-    data = data.asDict()
-    b_uuid = data['BuildingUUID']
-    sql = "SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem " \
-        "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
-    query = pd.read_sql(sql, ENGINE)
-    if not query.empty:
-        query["MeasuredBuildingArea"] = query.apply(
-            lambda x: float(x['MeasuredBuildingArea']) if x['MeasuredBuildingArea'] else 0.0,
-            axis=1)
-        data['BuildingArea'] = str(query['MeasuredBuildingArea'].sum())
+    # b_uuid = data['BuildingUUID']
+    # sql = "SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem " \
+    #     "WHERE HouseInfoItem.BuildingUUID = '{}'".format(b_uuid)
+    # query = pd.read_sql(sql, ENGINE)
+    # if not query.empty:
+    #     query["MeasuredBuildingArea"] = query.apply(
+    #         lambda x: float(x['MeasuredBuildingArea']) if x['MeasuredBuildingArea'] else 0.0,
+    #         axis=1)
+    #     data['BuildingArea'] = str(query['MeasuredBuildingArea'].sum())
 
-    data = Row(**data)
     return data
 
 
