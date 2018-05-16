849b4215d84463fd484e34509be8c3b73ffb5270
Gssssssssy <coder.gsy@gmail.com>
2018-05-16 18:16:11 +0800

    UDF

--- Src/SparkETLCore/CityCore/Xuzhou/PresellCoreUDF.py
+++ Src/SparkETLCore/CityCore/Xuzhou/PresellCoreUDF.py
@@ -1,189 +1,34 @@
-import datetime
-import sys
+# -*- coding: utf-8 -*-
+import demjson
+from pyspark.sql.types import StringType
+from pyspark.sql.functions import pandas_udf, PandasUDFType
 
-import pandas as pd
-from pyspark.sql import Row
-
-from Utils.Meth import cleanName, dateFormatter, getEngine
-
-sys.path.append('/home/lin/AwesomeSparkETL/Src/SparkETLCore')
-ENGINE = getEngine("spark_test")
+from SparkETLCore.Utils.Meth import cleanName
 
 
-def recordTime(data):
-    if not data.get('RecordTime'):
-        nt = datetime.datetime.now()
-        _row = data.asDict()
-        _row['RecordTime'] = nt
-        data = Row(**_row)
-    return data
+@pandas_udf(StringType())
+def record_time_clean(s):
+    import datetime
+    nt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    s = s.apply(lambda t: nt if not t else t)
+    return s
 
 
-def projectName(data):
-    return data
+@pandas_udf(StringType())
+def presale_permit_number_clean(s):
+    s = s.apply(lambda v: cleanName(v))
+    return s
 
 
-def realEstateProjectID(data):
-    _row = data.asDict()
-    _row['realEstateProjectID'] = _row.get('ProjectUUID', '')
-    data = Row(**_row)
-    return data
+@pandas_udf(StringType())
+def presale_building_amount_clean(s):
+    def reshape(value):
+        v_list = value.replace('、', ',').split(',')
+        v_len = len(set(v_list))
+        return str(v_len)
 
-
-def presalePermitNumber(data):
-    _pnum = data.get('PresalePermitNumber', '')
-    _row = data.asDict()
-    _pnum = cleanName(_pnum)
-    _row['PresalePermitNumber'] = _pnum
-    data = Row(**_row)
-    return data
-
-
-def totalBuidlingArea(data):
-    return data
-
-
-def approvalPresaleAmount(data):
-    return data
-
-
-def approvalPresaleArea(data):
-    return data
-
-
-def approvalPresaleHouseAmount(data):
-    data = data.asDict()
-    permit_num = data.get('PresalePermitNum', "")
-    sql = """
-    SELECT * FROM (SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem
-     JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
-     WHERE PresellInfoItem.PresalePermitNumber = "20140736" AND HouseInfoItem.HouseUseType = "住宅"
-    UNION ALL
-     SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem
-     JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
-     WHERE PresellInfoItem.PresalePermitNumber = "20140736" AND HouseInfoItem.HouseUseType = "商住"
-    UNION ALL
-     SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem
-     JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
-     WHERE PresellInfoItem.PresalePermitNumber = "20140736" AND HouseInfoItem.HouseUseType = "阁楼"
-    ) t
-    """.format(permit_num)
-    q = pd.read_sql(sql, ENGINE)
-    house_amount = 0
-    if not q.empty:
-        house_amount = q['MeasuredBuildingArea'].count()
-    data['presaleTotalBuildingArea'] = house_amount
-    data = Row(**data)
-    return data
-
-
-def approvalPresaleHouseArea(data):
-    data = data.asDict()
-    permit_num = data.get('PresalePermitNum', "")
-    sql = """
-    SELECT * FROM (SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem
-     JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
-     WHERE PresellInfoItem.PresalePermitNumber = "20140736" AND HouseInfoItem.HouseUseType = "住宅"
-    UNION ALL
-     SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem
-     JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
-     WHERE PresellInfoItem.PresalePermitNumber = "20140736" AND HouseInfoItem.HouseUseType = "商住"
-    UNION ALL
-     SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem
-     JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
-     WHERE PresellInfoItem.PresalePermitNumber = "20140736" AND HouseInfoItem.HouseUseType = "阁楼"
-    ) t
-    """.format(permit_num)
-    q = pd.read_sql(sql, ENGINE)
-    house_amount = 0
-    if not q.empty:
-        house_amount = q['MeasuredBuildingArea'].sum()
-    data['presaleTotalBuildingArea'] = house_amount
-    data = Row(**data)
-    return data
-
-
-def presaleBuildingAmount(data):
-    _row = data.asDict()
-    _pba = data.get('PresaleBuildingAmount', '')
-    _pba = str(len(set(_pba.replace('、', ',').split(','))))
-    _row['PresaleBuildingAmount'] = _pba
-    data = Row(**_row)
-    return data
-
-
-def constructionFloorCount(data):
-    return data
-
-
-def builtFloorCount(data):
-    data = data.asDict()
-    permit_num = data.get('PresalePermitNum', "")
-    sql = """
-    SELECT HouseInfoItem.Floor FROM HouseInfoItem
-    JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
-    WHERE PresellInfoItem.PresalePermitNumber = "{}"
-    """.format(permit_num)
-    q = pd.read_sql(sql, ENGINE)
-    b_count = 0
-    if not q.empty:
-        b_count = q['Floor'].unique().count()
-    data['BuiltFloorCount'] = b_count
-    data = Row(**data)
-    return data
-
-
-def periodsCount(data):
-    return data
-
-
-def constructionTotalArea(data):
-    return data
-
-
-def groundArea(data):
-    return data
-
-
-def underGroundArea(data):
-    return data
-
-
-def presaleTotalBuidlingArea(data):
-    data = data.asDict()
-    permit_num = data.get('PresalePermitNum', "")
-    sql = """
-    SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem
-    JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
-    WHERE PresellInfoItem.PresalePermitNumber = "{}"
-    """.format(permit_num)
-    q = pd.read_sql(sql, ENGINE)
-    tt_b_area = '0'
-    if not q.empty:
-        tt_b_area = '{}'.format(q['MeasuredBuildingArea'].sum())
-    data['presaleTotalBuildingArea'] = tt_b_area
-    data = Row(**data)
-    return data
-
-
-def contacts(data):
-    return data
-
-
-def presaleBuildingSupportingAreaInfo(data):
-    return data
-
-
-def presaleHousingLandIsMortgage(data):
-    return data
-
-
-def validityDateStartDate(data):
-    return data
-
-
-def validityDateClosingDate(data):
-    return data
+    s = s.apply(lambda v: reshape(v))
+    return s
 
 
 def lssueDate(data):
@@ -194,75 +39,27 @@ def lssueDate(data):
     return data
 
 
-def lssuingAuthority(data):
-    return data
+@pandas_udf(StringType())
+def land_use_clean(s):
+    s = s.apply(
+        lambda v: demjson.encode(v.replace('、', '/').replace('，', ',').split(','))
+    )
+    return s
 
 
-def presaleRegistrationManagementDepartment(data):
-    return data
-
-
-def validityDateDescribe(data):
-    return data
-
-
-def approvalPresalePosition(data):
-    return data
-
-
-def landUse(data):
-    _use = data['LandUse'].replace('、', '/').replace('，', ',').split(',')
-    _row = data.asDict()
-    _row['LandUse'] = _use
-    data = Row(**_row)
-    return data
-
-
-def earliestStartDate(data):
-    return data
-
-
-def latestDeliversHouseDate(data):
-    return data
-
-
-def earliestOpeningDate(data):
-    return data
-
-
-def houseSpread(data):
-    return data
-
-
-def presalePermitTie(data):
-    return data
-
-
-def presaleHouseCount(data):
-    data = data.asDict()
-    permit_num = data.get('PresalePermitNum', "")
-    sql = """
-    SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem
-    JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
-    WHERE PresellInfoItem.PresalePermitNumber = "{}"
-    """.format(permit_num)
-    q = pd.read_sql(sql, ENGINE)
-    h_count = 0
-    if not q.empty:
-        h_count = q['MeasuredBuildingArea'].count()
-    data['presaleHouseCount'] = h_count
-    data = Row(**data)
-    return data
-
-
-def remarks(data):
-    _extraj = data.get('ExtraJson')
-    if _extraj:
-        _row = data.asDict()
-        _row['rmarks'] = _extraj.get('ExtraPresellUUID', '')
-        data = Row(**_row)
-    return data
-
-
-def sourceUrl(data):
-    return data
+# 直接 .agg('count')
+# def presaleHouseCount(data):
+#     data = data.asDict()
+#     permit_num = data.get('PresalePermitNum', "")
+#     sql = """
+#     SELECT HouseInfoItem.MeasuredBuildingArea FROM HouseInfoItem
+#     JOIN PresellInfoItem ON HouseInfoItem.ProjectUUID = PresellInfoItem.ProjectUUID
+#     WHERE PresellInfoItem.PresalePermitNumber = "{}"
+#     """.format(permit_num)
+#     q = pd.read_sql(sql, ENGINE)
+#     h_count = 0
+#     if not q.empty:
+#         h_count = q['MeasuredBuildingArea'].count()
+#     data['presaleHouseCount'] = h_count
+#     data = Row(**data)
+#     return data
