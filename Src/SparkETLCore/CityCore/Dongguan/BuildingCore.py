91ce45820c7d638e5642a0a682933be9846925f4
孙嘉嘉 <jiajia.sun@yunfangdata.com>
2018-05-17 14:50:38 +0800

    修改building表JDBC

--- Src/SparkETLCore/CityCore/Dongguan/BuildingCore.py
+++ Src/SparkETLCore/CityCore/Dongguan/BuildingCore.py
@@ -1,13 +1,6 @@
 # coding=utf-8
 from __future__ import division
-import re
-import sys
-import inspect
-import pandas as pd
-import numpy as np
 import datetime
-
-from pyspark.sql import Row
 from SparkETLCore.Utils import Var, Meth, Config
 
 METHODS = ['address',
@@ -43,8 +36,6 @@ METHODS = ['address',
 
 
 def recordTime(data):
-    # print(data, inspect.stack()[0][3])
-
     nowtime = str(datetime.datetime.now())
     if data['RecordTime'] == '':
         data['RecordTime'] = nowtime
@@ -52,149 +43,102 @@ def recordTime(data):
 
 
 def projectName(data):
-    # print(data, inspect.stack()[0][3])
-
-    data['ProjectName'] = Meth.cleanName(data['ProjectName'])
     return data
 
 
 def realEstateProjectId(data):
-    # print(data, inspect.stack()[0][3])
-
-    data['RealEstateProjectID'] = data['ProjectUUID']
     return data
 
 
 def buildingName(data):
-    # print(data, inspect.stack()[0][3])
-
-    data['BuildingName'] = Meth.cleanName(data['BuildingName'])
     return data
 
 
 def buildingId(data):
-    # print(data, inspect.stack()[0][3])
-
-    data['BuildingID'] = data['BuildingID']
     return data
 
 
 def buildingUUID(data):
-    # print(data, inspect.stack()[0][3])
-
-    data['BuildingUUID'] = data['BuildingUUID']
     return data
 
 
 def unitName(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def unitId(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def presalePermitNumber(data):
-    # print(data, inspect.stack()[0][3])
-    # 
-    # df = pd.read_sql(con=Var.ENGINE,
-    #                  sql=u"select PresalePermitNumber as col from ProjectInfoItem where City='东莞' and ProjectName='{projectName}' order by RecordTime".format(
-    #                      projectName=data['ProjectName']))
-    # data['PresalePermitNumber'] = df.col.values[-1] if not df.empty else ''
     return data
 
 
 def address(data):
-    # print(data, inspect.stack()[0][3])
-    # 
-    # df = pd.read_sql(con=Var.ENGINE,
-    #                  sql=u"select ProjectAddress as col from ProjectInfoItem where City='东莞' and ProjectName='{projectName}' order by RecordTime".format(
-    #                      projectName=data['ProjectName']))
-    # data['Address'] = df.col.values[-1] if not df.empty else ''
     return data
 
 
 def onTheGroundFloor(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def theGroundFloor(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def estimatedCompletionDate(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def housingCount(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def floors(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def elevatorHouse(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def isHasElevator(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def elevaltorInfo(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def buildingStructure(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def buildingType(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def buildingHeight(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def buildingCategory(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def units(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def unsoldAmount(data):
-    # print(data, inspect.stack()[0][3])
-
-    data['UnsoldAmount'] = data['UnsoldAmount']
     return data
 
 
 def buildingAveragePrice(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def buildingPriceRange(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
@@ -203,17 +147,14 @@ def buildingArea(data):
 
 
 def remarks(data):
-    # print(data, inspect.stack()[0][3])
     return data
 
 
 def sourceUrl(data):
-    # print(data, inspect.stack()[0][3])
     data['SourceUrl'] = str(Meth.jsonLoad(
         data['ExtraJson']).get('ExtraSourceUrl', ''))
     return data
 
 
 def extrajson(data):
-    # print(data, inspect.stack()[0][3])
     return data
