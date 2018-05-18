90ee1d24c4b3f4188fd6a9fabd526704eb0749a3
Gssssssssy <coder.gsy@gmail.com>
2018-05-18 11:13:24 +0800

    UDF

--- Src/SparkETLCore/CityCore/Xuzhou/QuitCaseCoreUDF.py
+++ Src/SparkETLCore/CityCore/Xuzhou/QuitCaseCoreUDF.py
@@ -1,207 +1,103 @@
 # -*- coding: utf-8 -*-
-from pyspark.sql import Row
-
-
-def realEstateProjectId(data):
-    return data
-
-
-def buildingId(data):
-    return data
-
-
-def houseId(data):
-    return data
-
-
-def forecastBuildingArea(data):
-    return data
-
-
-def forecastInsideOfBuildingArea(data):
-    return data
-
-
-def forecastPublicArea(data):
-    return data
-
-
-def measuredBuildingArea(data):
-    return data
-
-
-def measuredInsideOfBuildingArea(data):
-    return data
-
-
-def measuredSharedPublicArea(data):
-    return data
-
-
-def isMortgage(data):
-    return data
-
-
-def isAttachment(data):
-    return data
-
-
-def isPrivateUse(data):
-    return data
-
-
-def isMoveBack(data):
-    return data
-
-
-def isSharedPublicMatching(data):
-    return data
-
-
-def buildingStructure(data):
-    data = data.asDict()
-    data['BuildingStructure'] = data['BuildingStructure'].replace('钢混', '钢混结构') \
-                                                         .replace('框架', '框架结构') \
-                                                         .replace('钢筋混凝土', '钢混结构') \
-                                                         .replace('混合', '混合结构') \
-                                                         .replace('砖混', '砖混结构') \
-                                                         .replace('框剪', '框架剪力墙结构') \
-                                                         .replace('结构结构', '结构') \
-                                                         .replace('钢、', '')
-    data = Row(**data)
-
-    return data
-
-
-def sellSchedule(data):
-    return data
-
-
-def sellState(data):
-    return data
-
-
-def sourceLink(data):
-    return data
-
-
-def caseTime(data):
-    return data
-
-
-def caseFrom(data):
-    return data
-
-
-def unitShape(data):
-    return data
-
-
-def unitStructure(data):
-    return data
-
-
-def balconys(data):
-    return data
-
-
-def unEnclosedBalconys(data):
-    return data
-
-
-def districtName(data):
-    data = data.asDict()
-    data['DistrictName'] = data['DistrictName'].replace('金山桥经济开发区', '经济技术开发区')
-    data = Row(**data)
-
-    return data
-
-
-def regionName(data):
-    return data
-
-
-def projectName(data):
-    return data
-
-
-def buildingName(data):
-    return data
-
-
-def presalePermitNumber(data):
-    data = data.asDict()
-    data['PresalePermitNumber'] = data['PresalePermitNumber'].replace(
-        '（', '(').replace('）', ')')
-
-    return data
-
-
-def houseName(data):
-    data = data.asDict()
-    data['HouseName'] = data['UnitName'] + '单元' \
-        + data['Floor'] + '层' + str(data['HouseNumber'])
-    data = Row(**data)
-
-    return data
-
-
-def totalPrice(data):
-    return data
+import demjson
+from pyspark.sql.types import StringType
+from pyspark.sql.functions import pandas_udf
+
+
+@pandas_udf(StringType())
+def city_clean(s):
+    s = s.apply(lambda v: '徐州')
+    return s
+
+
+# ProjectInfoItem
+@pandas_udf(StringType())
+def district_name_apply(s):
+    def func(v):
+        r = [i for i in v if i != '']
+        r = list(set(','.join(r).split(',')))
+        return r[0].replace('金山桥经济开发区', '经济技术开发区')
+
+    s = s.apply(lambda x: fun(x))
+    return s
+
+
+# ProjectInfoItem
+@pandas_udf(StringType())
+def address_apply(s):
+    s = s.apply(lambda v: v[0])
+    return s
+
+
+@pandas_udf(StringType())
+def building_structure_clean(s):
+    s = s.apply(
+        lambda x: x.replace('钢混', '钢混结构') \
+        .replace('框架', '框架结构') \
+        .replace('钢筋混凝土', '钢混结构') \
+        .replace('混合', '混合结构') \
+        .replace('结构结构', '结构') \
+        .replace('砖混', '砖混结构') \
+        .replace('框剪', '框架剪力墙结构') \
+        .replace('钢、', '')
+    )
+    return s
+
+
+@pandas_udf(StringType())
+def presale_permit_number_clean(s):
+    s = s.apply(lambda v: v.replace('（', '(').replace('）', ')'))
+    return s
 
 
 def price(data):
-    return data
-
-
-def priceType(data):
     data = data.asDict()
-    data['PriceType'] = '项目均价'
-    data = Row(**data)
+    pid = data['ProjectUUID']
+    sql = """
+    SELECT ProjectInfoItem.AveragePrice FROM ProjectInfoItem
+    WHERE ProjectInfoItem.ProjectUUID != {}
+    """.format(pid)
     return data
 
 
-def address(data):
-    return data
+@pandas_udf(StringType())
+def state_extract(s):
+    def func(value):
+        if values == '可销售':
+            return '明确成交'
+        else:
+            return '历史成交'
+
+    s = s.apply(lambda v: func(v))
+    return s
 
 
-def buildingCompletedYear(data):
-    return data
+# price = ProjeDF['AveragePrice'][(ProjeDF.ProjectUUID==temple['ProjectUUID'])&(ProjeDF.RecordTime <= deal_case.casetime)]
+# districtname = ProjeDF['DistrictName'][(ProjeDF.ProjectUUID==temple['ProjectUUID'])&(ProjeDF.DistrictName !='')]
+# address = ProjeDF['ProjectAddress'][(ProjeDF.ProjectUUID==temple['ProjectUUID'])&(ProjeDF.ProjectAddress!='')]
+# regionname = PresellInfoDF['ExtraRegionName'][(PresellInfoDF.PresalePermitNumber==temple['PresalePermitNumber'])&(PresellInfoDF.ExtraRegionName !='')]
 
 
-def floor(data):
-    return data
+# PresellInfoItem
+@pandas_udf(StringType())
+def region_name_extract(s):
+    s = s.apply(lambda j: demjson.decode(j).get('ExtraRegionName', ''))
+    return s
 
 
-def nominalFloor(data):
-    return data
+@pandas_udf(StringType())
+def region_name_apply(s):
+    def func(v):
+        r = [i for i in v if i != '']
+        r = list(set(','.join(r).split(',')))
+        return r[0]
+
+    s = s.apply(lambda x: func(x))
+    return s
 
 
-def floors(data):
-    return data
-
-
-def houseUseType(data):
-    return data
-
-
-def dwelling(data):
-    return data
-
-
-def remarks(data):
-    return data
-
-
-def houseNumber(data):
-    return data
-
-
-def state(data):
-    data = data.asDict()
-    if data['HouseStateLatest'] == '可销售':
-        data['State'] = '明确成交'
-    else:
-        data['State'] = '历史成交'
-    data = Row(**data)
-    return data
+# PresalePermitNumber
+@pandas_udf(StringType())
+def presale_permit_number_clean(s):
+    s = s.apply(lambda v: cleanName(v))
+    return s
